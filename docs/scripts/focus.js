b598795b31a1c4a2ba7deddc84494d0c396bb9f9
fabricetheytaz <fabrice.theytaz@gmail.com>
2026-08-05 19:43:33 +0200

    CoC

--- docs/scripts/focus.js
+++ docs/scripts/focus.js
@@ -1,2 +1,8 @@
-const F = [0.5, 0.7, 1, 1.4, 2, 2.8, 4, 5.6, 8, 11, 16, 22, 32, 45, 64, 90, 128, 180, 256, 360, 512];
-export { F };
+//const F = [0.5, 0.7, 1, 1.4, 2, 2.8, 4, 5.6, 8, 11, 16, 22, 32, 45, 64, 90, 128, 180, 256, 360, 512];
+function getSensorDiagonal(sensor) {
+    return Math.sqrt(sensor.width ** 2 + sensor.height ** 2);
+}
+function getCircleOfConfusion(sensor, distance, dpi = null) {
+    return getSensorDiagonal(sensor) / distance;
+}
+export { getSensorDiagonal, getCircleOfConfusion };
--- docs/src/focus.ts
+++ docs/src/focus.ts
@@ -1,7 +1,28 @@
 
-const F = [0.5, 0.7, 1, 1.4, 2, 2.8, 4, 5.6, 8, 11, 16, 22, 32, 45, 64, 90, 128, 180, 256, 360, 512];
+//const F = [0.5, 0.7, 1, 1.4, 2, 2.8, 4, 5.6, 8, 11, 16, 22, 32, 45, 64, 90, 128, 180, 256, 360, 512];
+
+type Sensor =
+	{
+	width:number;
+	height:number;
+	};
+
+function getSensorDiagonal(sensor:Sensor):number
+	{
+	return Math.sqrt(sensor.width**2 + sensor.height**2);
+	}
+
+function getCircleOfConfusion(sensor:Sensor, distance:number, dpi:number|null = null):number
+	{
+	return getSensorDiagonal(sensor) / distance;
+	}
 
 export
 	{
-    F
+	//F,
+
+	type Sensor,
+
+	getSensorDiagonal,
+	getCircleOfConfusion
 	};
